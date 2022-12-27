72b9c832a88a175acd21683df511da10d7c7cf47
ravivaghela0798 <vaghelarv600@gmail.com>
2022-12-27 19:11:47 +0530

    validation

--- src/app/create-customer/create-customer.component.ts
+++ src/app/create-customer/create-customer.component.ts
@@ -12,10 +12,9 @@ import { first } from 'rxjs';
 })
 export class CreateCustomerComponent implements OnInit {
 
-  custommers: Customer = new Customer();
   id: number;
   isCreate: Boolean;;
-
+  submitted = false;
   customer: FormGroup;
 
   constructor(
@@ -32,25 +31,21 @@ export class CreateCustomerComponent implements OnInit {
 
 
     this.customer = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
+      id: [this.id, Validators.required],
+      firstName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+      lastName: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
       dateOfBirth: ['', Validators.required],
-      mobile: ['', Validators.required],
-      address1: ['', Validators.required],
-      address2: ['', Validators.required],
-      age: ['', Validators.required],
+      mobile: ['', [Validators.minLength(5), Validators.maxLength(17), Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
+      address1: ['', [Validators.minLength(1), Validators.maxLength(100)]],
+      address2: ['', [Validators.minLength(5), Validators.maxLength(100)]],
+      age: ['', [Validators.minLength(10), Validators.maxLength(120)]],
       gender: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', [Validators.required, Validators.email, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
     })
 
     if (!this.isCreate) {
       this.getById();
     }
-
-  }
-
-  confirm(){
-
   }
 
   private getById() {
@@ -76,15 +71,22 @@ export class CreateCustomerComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.isCreate) {
-      this.save();
-    } else {
-      this.update();
+    this.submitted = true;
+    if (this.customer.invalid) {
+      return
+    }
+    else {
+      if (this.isCreate) {
+        this.save();
+        console.log(this.customer);
+      } else {
+        this.update();
+      }
     }
   }
 
   cancel() {
-      this.router.navigateByUrl('/customers');
+    this.router.navigateByUrl('/customers');
   }
 
 }
